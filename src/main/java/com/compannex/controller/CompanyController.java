614eea2c99f74359057bd472c12fb499e7c2edae
Hayk <haik.hovsepian@gmail.com>
2012-01-09 12:34:25 +0400

    added MVC

---
+++ src/main/java/com/compannex/controller/CompanyController.java
@@ -0,0 +1,14 @@
+package com.compannex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CompanyController {
+
+	
+	@RequestMapping("/searchCompanies")
+	public void searchCompanies() {
+		
+	}
+}
