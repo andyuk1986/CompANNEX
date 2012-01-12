7997da50d6d573565394bf9616540d0eb97b1ae0
Hayk <haik.hovsepian@gmail.com>
2012-01-12 21:28:35 +0400

    added jsp pages

--- src/main/java/com/compannex/controller/HomeController.java
+++ src/main/java/com/compannex/controller/HomeController.java
@@ -4,14 +4,19 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
 	private static Logger logger = Logger.getLogger(HomeController.class);
 
-	@RequestMapping("/index.jsp")
-	public void home(@RequestParam String defaultParam) {
-		logger.info("!!!defaultParam" + defaultParam);
+	@RequestMapping("/home.do")
+	public ModelAndView home() {
+		logger.info("Home");
+		
+		ModelAndView result = new ModelAndView("index");
+		
+		return result;
 	}
 }
